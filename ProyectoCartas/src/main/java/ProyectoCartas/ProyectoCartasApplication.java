ae55e9a24af8ecf22154e94d9bebcf7213366edf
Sebastian Pino <se.pinob@duocuc.cl>
2025-05-14 16:11:12 -0400

    Add files via upload
    Esqueleto

---
+++ ProyectoCartas/src/main/java/ProyectoCartas/ProyectoCartasApplication.java
@@ -0,0 +1,13 @@
+package ProyectoCartas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCartasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCartasApplication.class, args);
+	}
+
+}
---
+++ ProyectoCartas/src/test/java/ProyectoCartas/ProyectoCartasApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoCartas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCartasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
