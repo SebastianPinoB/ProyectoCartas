ebcb3deb4b8fca0667a90c75514c64409525fba8
Pablo <pabl.sepulvedaa@duocuc.cl>
2025-05-22 11:14:52 -0400

    modelos realizados

---
+++ ProyectoCartas/src/main/java/ProyectoCartas/model/Cliente.java
@@ -0,0 +1,39 @@
+package ProyectoCartas.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "cliente")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
+
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCliente;
+
+    @Column(nullable = false, unique = true)
+    private String runCliente;
+
+    @Column(nullable = false)
+    private String nombreCliente;
+
+    @Column(nullable = false)
+    private String apellidoCliente;
+
+    @ManyToOne
+    @JoinColumn(name = "idCarta")
+    private List<Carta> cartasCompradas;
+
+    /*
+    @ManyToOne
+    @JoinColumn
+
+     */
+}
