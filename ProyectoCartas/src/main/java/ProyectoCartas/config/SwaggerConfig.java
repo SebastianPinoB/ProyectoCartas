9a46b168bb72ab7c1b1268f156e5e86d86346bbe
Gocabezon <go.cabezon@duocuc.cl>
2025-06-20 18:05:53 -0400

    OpenAPI avanzado, debería funcionar

--- ProyectoCartas/src/main/java/ProyectoCartas/config/SwaggerConfig.java
+++ ProyectoCartas/src/main/java/ProyectoCartas/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package ProyectoCartas.config;
 
+
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                        .title("ProyectoCartas")
+                        .version("1.0")
+                        .description("Documentación de la API para el sistema de carpeta digital de cartas"));
+    }
+
 }
