b5bb06cc0466b18c2c9ac76eebafab41d19bc82c
Pablo <pabl.sepulvedaa@duocuc.cl>
2025-06-20 22:41:32 -0400

    Agregado algunos test de clientes con explicacion de cada uno

---
+++ ProyectoCartas/src/test/java/ProyectoCartas/ClienteRepositoryTest.java
@@ -0,0 +1,44 @@
+package ProyectoCartas;
+
+import ProyectoCartas.modelo.Cliente;
+import ProyectoCartas.repository.ClienteRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@Commit
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
+class ClienteRepositoryTest {
+    @Autowired
+    private ClienteRepository clienteRepository;
+    private Cliente clienteGuardado;
+
+    @BeforeEach
+    void seed(){
+        clienteGuardado = clienteRepository.save(new Cliente(null, "21458421", "Pablo"));
+    }
+
+    @Test
+    void findbyId(){
+        Optional<Cliente> cliente = clienteRepository.findById(clienteGuardado.getIdCliente());
+
+        assertTrue(cliente.isPresent());
+        assertEquals("Pablo", cliente.get().getNombre());
+    }
+
+    @Test
+    void deletebyId(){
+        clienteRepository.deleteById(clienteGuardado.getIdCliente());
+    }
+}
---
+++ ProyectoCartas/src/test/java/ProyectoCartas/ClienteServiceTest.java
@@ -0,0 +1,62 @@
+package ProyectoCartas;
+
+import ProyectoCartas.modelo.Cliente;
+import ProyectoCartas.repository.ClienteRepository;
+import ProyectoCartas.service.ClienteService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ClienteServiceTest {
+    @Autowired
+    private ClienteService clienteService;
+
+    @MockitoBean
+    private ClienteRepository clienteRepository;
+
+    // El test funciona siempre y cuando el tamaño de la lista sea mayor a 0
+    @Test
+    public void testFindAll(){
+        when(clienteRepository.findAll()).thenReturn(List.of(new Cliente(1, "21951243", "Benjamin")));
+
+        List<Cliente> clientes = clienteService.listarClientes();
+
+        assertNotNull(clientes);
+        assertEquals(1, clientes.size());
+    }
+
+    @Test
+    public void testFindById(){
+        Integer codigo = 1;
+        Cliente cliente = new Cliente(codigo, "21951243", "Otro nombre");
+        when(clienteRepository.findById(codigo)).thenReturn(Optional.of(cliente));
+
+        Cliente encontrado = clienteService.encontrarClientePorId(codigo);
+
+        assertNotNull(encontrado);
+        assertEquals(codigo, encontrado.getIdCliente()); // si la variable codigo y el id del cliente coincide entonces termina correctamente
+
+        // assertEquals funciona con un argumento en la posicion 1, el cual es el esperado y el otro es el actual el cual esta siendo usado.
+    }
+
+    @Test
+    public void testSaveCliente(){
+        Cliente cliente = new Cliente(3, "21951243", "Otro nombre");
+        when(clienteRepository.save(cliente)).thenReturn(cliente);
+
+        Cliente guardado = clienteService.guardarCliente(cliente);
+        assertNotNull(guardado);
+        assertEquals("asdasdd", guardado.getNombre()); // el primer argumento coincide con el siguiente osea, el nombre del cliente guardado finaliza corretamente
+    }
+
+}
